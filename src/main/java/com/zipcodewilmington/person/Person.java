ad2dfd2ca517d8f6fb72d15933b1c327e1df6ccc
Isaac <isaac@Zipcoders-MacBook-Pro-2.local>
2025-03-05 17:51:00 -0500

    got most of the test to pass, constructor is still returning null

--- src/main/java/com/zipcodewilmington/person/Person.java
+++ src/main/java/com/zipcodewilmington/person/Person.java
@@ -6,30 +6,52 @@ package com.zipcodewilmington.person;
 public class Person {
     private String name;
     private int age;
+//    private int height;
+//    private String favFood;
+//    private boolean maritalStatus;
+//    private int numberOfKids;
+//    private String city;
 
-    public Person() {
+
+//    public Person(String name, int age, int height,String favFood, boolean maritalStatus, int numberOfKids, String city){
+    public Person(){
+        name =  this.name;
+       age = this.age;
+//        this.height = height;
+//        this.favFood = favFood;
+//        this.maritalStatus = maritalStatus;
+//        this.numberOfKids = numberOfKids;
+//        this.city = city;
     }
 
+
+
     public Person(int age) {
+        this.age = age;
     }
 
     public Person(String name) {
+        this.name = name;
     }
 
     public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
     }
 
     public void setName(String name) {
+        this.name = name;
     }
 
     public void setAge(int age) {
+        this.age = age;
     }
 
     public String getName() {
-        return null;
+        return name;
     }
 
     public Integer getAge() {
-        return null;
+        return age;
     }
 }
